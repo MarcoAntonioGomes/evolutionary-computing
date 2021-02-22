7e25e7f970186d4467ef53ffa2c07be1b248cc88
marcoantgomes <marcoant.gomes07@gmail.com>
2021-02-21 21:52:30 -0300

    PSO implementatio parcial

--- pso/constricted_pso.py
+++ pso/constricted_pso.py
@@ -9,18 +9,22 @@ developed by Marco Antônio Gomes
 '''
 
 
-def f1(x):
-    return 0
+class Particle:
 
+    def __init__(self, ):
+        self.position = np.random.randint(-100, 100, 10)
+        self.velocity = np.zeros(10)
+        self.best_part_pos = self.position
+        self.best_value = float('inf')
 
-def f2(x):
-    return 0
+    def move(self):
+        self.position = self.position + self.velocity
 
 
 class ConstrictedPso:
     random = SystemRandom()
 
-    def __init__(self, w, c1, c2, x, max_epochs, swarm, swarm_size, target, target_error):
+    def __init__(self, w, c1, c2, x, max_epochs, swarm, swarm_size, target, target_error, fitness_function):
         self.w = w
         self.c1 = c1
         self.c2 = c2
@@ -29,20 +33,21 @@ class ConstrictedPso:
         self.swarm = swarm
         self.swarm_size = swarm_size
         self.gbest_value = float('inf')
-        self.gbest_position = np.random.randint(-100, 100, 2)
+        self.gbest_position = np.random.randint(-100, 100, 10)
         self.target = target
         self.target_error = target_error
+        self.fitness_function = fitness_function
 
     def set_best(self):
         for particle in self.swarm:
-            fitness_value = f1(particle)
+            fitness_value = self.fitness_function(particle)
             if particle.best_value > fitness_value:
                 particle.best_value = fitness_value
                 particle.best_part_pos = particle.position
 
     def set_gbest(self):
         for particle in self.swarm:
-            best_fitness_candidate = f1(particle)
+            best_fitness_candidate = self.fitness_function(particle)
             if self.gbest_value > best_fitness_candidate:
                 self.gbest_value = best_fitness_candidate
                 self.gbest_position = particle.position
@@ -50,9 +55,9 @@ class ConstrictedPso:
     def move_particles(self):
 
         for particle in self.swarm:
-            new_velocity = self.x*((self.w * particle.velocity) + (self.c1 * self.random.random()) * (
-                        particle.best_part_pos - particle.position) +
-                                   (self.random.random() * self.c2) * (self.gbest_position - particle.position))
+            new_velocity = self.x * ((self.w * particle.velocity) + (self.c1 * self.random.random()) * (
+                    particle.best_part_pos - particle.position) +
+                                     (self.random.random() * self.c2) * (self.gbest_position - particle.position))
             particle.velocity = new_velocity
             particle.move()
 
@@ -67,16 +72,6 @@ class ConstrictedPso:
                 break
 
             self.move_particles()
-        epoch += 1
-
-
-class Particle:
-
-    def __init__(self, ):
-        self.position = np.random.randint(-100, 100, 2)
-        self.velocity = np.zeros(2)
-        self.best_part_pos = self.position
-        self.best_value = float('inf')
-
-    def move(self):
-        self.position = self.position + self.velocity
+            epoch += 1
+        print("A solução ótima é: ", self.gbest_position, " Época: ", epoch)
+        print("Gbest: ", self.gbest_value)
